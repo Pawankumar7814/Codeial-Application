52e21b56d4b0fcc81c39478d673e240e28765211
Pawankumar7814m <pawank4285@gmail.com>
2023-04-30 22:16:10 +0530

    commit Displayed the post and user data

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,6 +1,16 @@
-module.exports.home = function(req, res) {
-    return res.status(200).render('home', {
-        title: "Home - Codeial"
-    });
-    // return res.end('EXpress is up for codeial');
+// Import modules and models
+// const mongoose = require('../config/mongoose');
+const Post = require('../models/post');
+
+// Route to home page and parallely we are displaying the post and user data 
+module.exports.home = async function(req, res) {
+    try {
+        var post = await Post.find({}).populate('user').exec();
+        return res.status(200).render('home', {
+            title: "Home - Codeial",
+            posts: post
+        });
+    } catch (error) {
+        console.log(error);
+    }
 }

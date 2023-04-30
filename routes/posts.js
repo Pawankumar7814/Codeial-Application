295af7fa37e587d75a1c2aac60c8c10b865f0d7e
Pawankumar7814m <pawank4285@gmail.com>
2023-04-30 22:53:55 +0530

    commit changes by add restriction for add new post

--- routes/posts.js
+++ routes/posts.js
@@ -2,8 +2,9 @@
 const express = require('express');
 const router = express.Router();
 const postController = require('../controllers/post_controller');
+const passport = require('passport');
 
 // Route to user post page
-router.post('/add-new-post', postController.postCreate);
+router.post('/add-new-post', passport.checkAuthentication, postController.postCreate);
 
 module.exports = router;

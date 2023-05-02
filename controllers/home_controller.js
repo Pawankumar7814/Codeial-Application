ea185152cff1d48d777a833f8e79c6e305baf568
Pawankumar7814m <pawank4285@gmail.com>
2023-05-02 07:59:07 +0530

    add nested populating method for post and comments

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -5,13 +5,34 @@ const Comment = require('../models/comment');
 
 // Route to home page and parallely we are displaying the post and user data 
 module.exports.home = async function(req, res) {
+    // try {
+    //     var post = await Post.find({}).populate('user').exec();
+    //     var comment = await Comment.find({}).populate('post').exec();
+    //     return res.status(200).render('home', {
+    //         title: "Home - Codeial",
+    //         posts: post,
+    //         comments: comment
+    //     });
+    // } catch (error) {
+    //     console.log(error);
+    // }
+
+    // Nested population
     try {
-        var post = await Post.find({}).populate('user').exec();
-        var comment = await Comment.find({}).populate('post').exec();
+        var post = await Post.find({})
+            .populate('user')
+            .populate({
+                path: 'comment',
+                populate: {
+                    path: 'user'
+                }
+            })
+            .exec();
+        // var comment = await Comment.find({}).populate('post').exec();
         return res.status(200).render('home', {
             title: "Home - Codeial",
             posts: post,
-            comments: comment
+            comments: post
         });
     } catch (error) {
         console.log(error);
